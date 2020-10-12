a164e44706575d3b5eb5debe7642da3e9a50d43b
Alexander Dyachenka <alexdyachenka@gmail.com>
2020-10-12 20:02:58 +0300

    Adding BracketsWorker class with findMaxLevelBrackets function

--- Lab02/src/BracketsWorker.java
+++ Lab02/src/BracketsWorker.java
@@ -1,2 +1,55 @@
 public class BracketsWorker {
+    private int openBracketIndex;
+    private int closeBracketIndex;
+    private int maxBracketsLevel;
+
+
+
+    public int getMaxBracketsLevel(){
+        return maxBracketsLevel;
+    }
+
+    public BracketsWorker(){
+        openBracketIndex = 0;
+        closeBracketIndex = 0;
+        maxBracketsLevel = 0;
+    }
+    public void findMaxLevelBrackets(StringBuilder stringBuilder){
+        int bracketsLevel = 0;
+
+        for (int i = 0; i < stringBuilder.length(); i++){
+            if (stringBuilder.charAt(i) == '('){
+                bracketsLevel++;
+                boolean check = compareBracketsLevelWithMaxBracketsLevel(bracketsLevel);
+                changeMaxBracketsLevel(check, bracketsLevel);
+                changeOpenBracketIndex(check, i);
+            }
+            if (stringBuilder.charAt(i) == ')'){
+                boolean check = compareBracketsLevelWithMaxBracketsLevel(bracketsLevel);
+                changeCloseBracketIndex(check, i);
+                bracketsLevel--;
+            }
+        }
+    }
+    private boolean compareBracketsLevelWithMaxBracketsLevel(int bracketsLevel){
+        if (bracketsLevel >= maxBracketsLevel){
+            return true;
+        }
+        return false;
+    }
+    private void changeOpenBracketIndex(boolean flag, int index){
+        if (flag){
+            openBracketIndex = index;
+        }
+    }
+    private void changeCloseBracketIndex(boolean flag, int index){
+        if (flag){
+            closeBracketIndex = index;
+        }
+    }
+    private void changeMaxBracketsLevel(boolean flag, int bracketsLevel){
+        if (flag){
+            maxBracketsLevel = bracketsLevel;
+        }
+    }
 }
--- Lab02/src/StringWorker.java
+++ Lab02/src/StringWorker.java
@@ -1,8 +1,7 @@
 public class StringWorker {
     public static void main(String[] args){
         StringBuilder stringBuilder = new StringBuilder(args[0]);
-    }
-    private static void findBrackets(StringBuilder stringBuilder){
-
+        BracketsWorker bracketsWorker = new BracketsWorker();
+        bracketsWorker.findMaxLevelBrackets(stringBuilder);
     }
 }
