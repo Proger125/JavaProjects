36894eecb63ad0fe0c3034ab7fe51b7161393ccc
Alexander Dyachenka <alexdyachenka@gmail.com>
2020-10-12 19:13:42 +0300

    Starting Lab02

---
+++ Lab02/src/StringWorker.java
@@ -0,0 +1,8 @@
+public class StringWorker {
+    public static void main(String[] args){
+        StringBuilder stringBuilder = new StringBuilder(args[0]);
+    }
+    private static void findBrackets(StringBuilder stringBuilder){
+
+    }
+}
