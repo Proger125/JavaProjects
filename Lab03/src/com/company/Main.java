7c22c8243759bdafad2243f95c587f4d43239da3
Alexander Dyachenka <alexdyachenka@gmail.com>
2020-10-20 20:38:53 +0300

    Lab03 final

--- Lab03/src/com/company/Main.java
+++ Lab03/src/com/company/Main.java
@@ -1,14 +1,24 @@
 package com.company;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         MatrixReader reader = new MatrixReader();
         try {
             Matrix matrix = reader.ReadMatrixFromFile("./resources/Matrix.txt");
-        }catch (IOException exception){
+            Matrix resultMatrix = matrix.Gauss(3);
+            resultMatrix.MatrixOut();
+            ArrayList<Double> list = matrix.ScalarMult();
+            System.out.println("The scalar multiplications of rows and columns with min and max elements");
+            for (Double aDouble : list) {
+                System.out.print(aDouble + " ");
+            }
+        }catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }catch (ArithmeticException exception){
             System.out.println(exception.getMessage());
         }
     }
--- Lab03/src/com/company/Matrix.java
+++ Lab03/src/com/company/Matrix.java
@@ -1,11 +1,13 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Matrix {
     // fields
-    private double[][] data;
+    private final double[][] data;
     private int m;
     private int n;
-    private boolean isSquare;
+    private final boolean isSquare;
 
 
     // properties
@@ -19,7 +21,7 @@ public class Matrix {
         return n;
     }
     public void SetN(int value){
-        this.m = value;
+        this.n = value;
     }
     public double GetElement(int i, int j){
         return data[i][j];
@@ -43,21 +45,13 @@ public class Matrix {
         m = row;
         n = col;
         data = new double[m][n];
-        if (m == n){
-            isSquare = true;
-        }else{
-            isSquare = false;
-        }
+        isSquare = m == n;
     }
     public Matrix(double[][] array){
         m = array.length;
         n = array[0].length;
         data = array;
-        if (n == m){
-            isSquare = true;
-        }else{
-            isSquare = false;
-        }
+        isSquare = n == m;
     }
     public Matrix(Matrix matrix){
         m = matrix.GetM();
@@ -65,5 +59,115 @@ public class Matrix {
         data = matrix.data;
         isSquare = matrix.IsMatrixSquare();
     }
+    public Matrix Gauss(int amountOfAnswerVectors) throws ArrayIndexOutOfBoundsException, ArithmeticException{
+        if (amountOfAnswerVectors > this.m){
+            throw new ArrayIndexOutOfBoundsException("The number of answer vectors is incorrect");
+        }
 
+        int size = this.m; // размерность матрицы левой части
+
+        // прямой ход
+        for (int k = 0; k < size; k++){
+            double diagonalElement = data[k][k];
+            if (diagonalElement == 0){
+                throw new ArithmeticException("The determinant of your matrix = 0");
+            }
+            for (int i = 0; i < size + amountOfAnswerVectors; i++){
+                data[k][i] = data[k][i] / diagonalElement;
+            }
+            for (int i = k + 1; i < size; i++){
+                double K = data[i][k] / diagonalElement;
+                for (int j = 0; j < size + amountOfAnswerVectors; j++){
+                    data[i][j] = data[i][j] - data[k][j] * K;
+                }
+            }
+        }
+
+        // обратный ход
+        for (int k = size - 1; k > -1; k--){
+            double diagonalElement = data[k][k];
+            for (int i = size - 1 + amountOfAnswerVectors; i > -1; i--){
+                data[k][i] = data[k][i] / diagonalElement;
+            }
+            for (int i = k - 1; i > -1; i--){
+                double K = data[i][k] / diagonalElement;
+                for (int j = size - 1 + amountOfAnswerVectors; j > -1; j--){
+                    data[i][j] = data[i][j] - data[k][j] * K;
+                }
+            }
+        }
+        Matrix resultMatrix = new Matrix(this.m, amountOfAnswerVectors);
+        for (int i = 0; i < resultMatrix.m; i++){
+            for (int j = 0; j < resultMatrix.n; j++){
+                resultMatrix.SetElement(i,j, data[i][this.n - amountOfAnswerVectors + j]);
+            }
+        }
+        return resultMatrix;
+    }
+    public void MatrixOut(){
+        for (int i = 0; i < this.m; i++){
+            for (int j = 0; j < this.n; j++){
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public ArrayList<Double> ScalarMult(){
+        if (isSquare){
+            ArrayList<Integer> indexRows = MinElementsRows();
+            ArrayList<Integer> indexCols = MaxElementsCols();
+            ArrayList<Double> resultList = new ArrayList<Double>();
+            for (Integer indexRow : indexRows) {
+                for (Integer indexCol : indexCols) {
+                    double result = 0.0;
+                    for (int i = 0; i < m; i++) {
+                        result += data[indexRow][i] * data[i][indexCol];
+                    }
+                    resultList.add(result);
+                }
+            }
+
+            return  resultList;
+        }else{
+            throw new ArithmeticException("Your matrix should me square!!!");
+        }
+
+    }
+    private ArrayList<Integer> MinElementsRows(){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        double minElement = data[0][0];
+        for (double[] datum : data) {
+            for (int j = 0; j < data[0].length; j++) {
+                if (datum[j] < minElement) {
+                    minElement = datum[j];
+                }
+            }
+        }
+        list = AddToArrayListIfEqualTo(minElement);
+        return list;
+    }
+    private ArrayList<Integer> MaxElementsCols(){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        double maxElement = data[0][0];
+        for (double[] datum : data) {
+            for (int j = 0; j < data[0].length; j++) {
+                if (datum[j] > maxElement) {
+                    maxElement = datum[j];
+                }
+            }
+        }
+        list = AddToArrayListIfEqualTo(maxElement);
+        return list;
+    }
+    private ArrayList<Integer> AddToArrayListIfEqualTo(double element){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (double[] datum : data) {
+            for (int j = 0; j < data[0].length; j++) {
+                if (datum[j] == element) {
+                    list.add(j);
+                }
+            }
+        }
+        return list;
+    }
 }
